c368b1c172ee529d5df14d0ac79269664c8bbfc0
Josh <joshunhappy@gmail.com>
2023-11-19 22:16:44 -0600

    Adds comments on PolicyFactory

--- scheduler/scheduling/policies/PolicyFactory.java
+++ scheduler/scheduling/policies/PolicyFactory.java
@@ -9,15 +9,27 @@
 package scheduler.scheduling.policies;
 
 public class PolicyFactory {
+
+    // Este metodo crea y retorna una instancia a calendarizar basandose en los parametros
+    // Utiliza la variable policyType para decidir el tipo de politica
     public static Policy createPolicy(String policyType, boolean dualProcessor, long quantum) {
+        // Chequea si policyType es Firs Come First Served (FCFS)
         if ("-fcfs".equals(policyType)) {
             return new FCFSPolicy();
+
+        // Chequea si policyType es Last Come First Served (LCFS)
         } else if ("-lcfs".equals(policyType)) {
             return new LCFSPolicy();
+
+        // Chequea si policyType es Round Robin (RR)
         } else if ("-rr".equals(policyType)) {
             return new RRLPolicy(dualProcessor, quantum);
+
+        // Chequea si policyType es Priority Policy (PP)
         } else if ("-pp".equals(policyType)) {
             return new PriorityPolicy();
+
+        // Devuelve un mensaje de error si el string de policyType no es reconocido
         } else {
             throw new IllegalArgumentException("Tipo de politica invalido: " + policyType);
         }
