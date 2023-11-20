6cf034330f8a12283bd1cccf71c12fd71b8d35e6
Josh <joshunhappy@gmail.com>
2023-11-19 22:58:30 -0600

    Add comments to Processor.java

--- scheduler/Processor.java
+++ scheduler/Processor.java
@@ -4,15 +4,20 @@ package scheduler;
 
 import scheduler.processing.SimpleProcess;
 
+// Esta clase simula el procesamiento de un pproceso en un procesador
+
 public class Processor {
+    // Metodo estatico que simula el procesamiento de un proceso
     public static void process(SimpleProcess process) {
         try {
-            // Simulate processing time by sleeping for the specified duration
-            System.out.println("ProcessingTime: " + process.getProcessingTime() * 1000);
-            Thread.sleep(process.getProcessingTime());
+            // Simula el tiempo de procesamiento al dormir el hilo durante la duracion del tiempo del proceso
+            System.out.println("Tiempo de Proceso: " + process.getProcessingTime() * 1000);
+            // Tiempo de proceso en milisegundos
+            Thread.sleep(process.getProcessingTime() * 1000);
         } catch (InterruptedException e) {
+            // Maneja la interrupcion del hilo, imprimiendo un mensaje de error
             Thread.currentThread().interrupt();
-            System.err.println("Processing interrupted: " + e.getMessage());
+            System.err.println("Proceso Interrumpido: " + e.getMessage());
         }
     }
 }
